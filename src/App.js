13430c35df38e26cc9028554a03d9561c1669ea6
haobui14 <62874724+haobui14@users.noreply.github.com>
2022-03-27 17:40:22 -0500

    update card and card-list components

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends Component {
         <SearchBox
           onChangeHandler={onSearchChange}
           placeholder="search monsters"
-          className="search-box"
+          className="monsters-search-box"
         />
         <CardList monsters={filteredMonsters} />
       </div>
