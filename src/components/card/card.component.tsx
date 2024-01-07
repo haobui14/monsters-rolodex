3ec80eecffaf33ef5e57fc6e6541d462d5b59e0a
Hao Bui <haobp.dev@gmail.com>
2024-01-07 01:54:09 -0600

    convert from js to typescript

--- src/components/card/card.component.tsx
+++ src/components/card/card.component.tsx
@@ -1,9 +1,15 @@
-import "./card.styles.css";
-const Card = ({ monster }) => {
+import { Monster } from '../../App';
+
+import './card.styles.css';
+
+type CardProps = {
+  monster: Monster;
+};
+const Card = ({ monster }: CardProps) => {
   const { id, name, email } = monster;
 
   return (
-    <div className="card-container" key={id}>
+    <div className='card-container' key={id}>
       <img
         alt={`monster ${name}`}
         src={`https://robohash.org/${id}?set=set2&size=180x180`}
